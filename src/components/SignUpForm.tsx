45ccc2ccf122e9a87e300f995ae0f5ec945a7986
valescos <valescos1986@gmail.com>
2024-07-16 20:11:36 +0700

    Show/hide password added

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   getAuthToken,
   setConfirmPassword,
@@ -10,6 +11,10 @@ import { useAppDispatch, useAppSelector } from "../store/store";
 import { cn } from "../utilis";
 
 export default function SignUpForm() {
+  const [passwordShow, setPasswordsShow] = useState({
+    pass: false,
+    conf: false,
+  });
   const { name, email, password, confirm_password, errors } = useAppSelector(
     (state) => state.form
   );
@@ -68,15 +73,15 @@ export default function SignUpForm() {
           </p>
         )}
       </div>
-      <div>
+      <div className="relative">
         <label htmlFor="pass">Пароль</label>
         <input
           className={cn(
-            "rounded-md",
+            "rounded-md pr-6",
             errors.password && "border-rose-600 border-2"
           )}
           id="pass"
-          type="password"
+          type={passwordShow.pass ? "text" : "password"}
           value={password}
           onChange={(e) => dispatch(setPassword(e.target.value))}
         />
@@ -85,16 +90,27 @@ export default function SignUpForm() {
             Пароль должен состоять минимум из шести символов
           </p>
         )}
+        <span
+          onClick={() =>
+            setPasswordsShow({
+              ...passwordShow,
+              pass: !passwordShow.pass,
+            })
+          }
+          className="absolute cursor-pointer right-0 px-2 top-12"
+        >
+          {passwordShow.pass ? colsedeye : openeye}
+        </span>
       </div>
-      <div>
+      <div className="relative">
         <label htmlFor="pass_confirm">Подтвердите пароль</label>
         <input
           className={cn(
-            "rounded-md",
+            "rounded-md pr-6",
             errors.confirm_password && "border-rose-600 border-2"
           )}
           id="pass_confirm"
-          type="password"
+          type={passwordShow.conf ? "text" : "password"}
           value={confirm_password}
           onChange={(e) => dispatch(setConfirmPassword(e.target.value))}
         />
@@ -103,6 +119,17 @@ export default function SignUpForm() {
             Пароль не совпадает с подтверждением
           </p>
         )}
+        <span
+          onClick={() =>
+            setPasswordsShow({
+              ...passwordShow,
+              conf: !passwordShow.conf,
+            })
+          }
+          className="absolute cursor-pointer right-0 px-2 top-12"
+        >
+          {passwordShow.conf ? colsedeye : openeye}
+        </span>
       </div>
       <button className="bg-[#512689] px-4 py-2 text-white text-center rounded-md">
         Зарегистрироваться
@@ -110,3 +137,42 @@ export default function SignUpForm() {
     </form>
   );
 }
+
+const openeye = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+    />
+  </svg>
+);
+
+const colsedeye = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+    />
+  </svg>
+);
